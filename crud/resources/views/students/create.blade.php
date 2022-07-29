16936013261db77d1d06557340baaed85bdbaffa
EleazarSF <eleazarsf@gmail.com>
2022-07-29 07:54:07 -0300

    validacoes adicionadas

--- crud/resources/views/students/create.blade.php
+++ crud/resources/views/students/create.blade.php
@@ -9,23 +9,23 @@
         <div class='card-body'>
             <form action="{{url('student')}}" method='post'>
                 {!! csrf_field() !!}
-                <label for="">Nome</label>
-                <input minlength="3" maxlength="255" type="text" name='name' id='name' class='form-control' required>
-                <br>
-                <label for="">Endereço</label>
-                <input maxlength="255" type="text" name='adress' id='adress' class='form-control' required>
+                <label for="">Nome completo</label>
+                <input minlength="3" maxlength="255" type="text" name='name' value="{{old('name')}}" id='name' class='form-control' required>
                 <br>
                 <label for="">E-mail</label>
-                <input minlength="6" maxlength="255" type="email" name='email' id='email' class='form-control' placeholder="exemplo@email.com" required>
+                <input minlength="6" maxlength="255" type="email" name='email' value="{{old('email')}}" id='email' class='form-control' placeholder="exemplo@email.com" required>
                 <br>
                 <label for="">Telefone</label>
-                <input minlength="11" maxlength="14" type="tel" name='mobile' id='mobile' class='form-control' placeholder="(00) 00000 - 0000" required>
+                <input minlength="11" maxlength="14" type="tel" name='mobile' value="{{old('mobile')}}" id='mobile' class='form-control' placeholder="(00) 00000 - 0000" required>
+                <br>
+                <label for="">Endereço</label>
+                <input minlength="3" maxlength="255" type="text" name='adress' value="{{old('adress')}}" id='adress' class='form-control' required>
                 <br>
                 <label for="">CPF</label>
-                <input minlength="11" maxlength="11" type="text" name='cpf' id='cpf' class='form-control' placeholder="000.000.000-00" required>
+                <input minlength="11" maxlength="11" type="text" name='cpf' value="{{old('cpf')}}" id='cpf' class='form-control' placeholder="000.000.000-00" required>
                 <br>
                 <label for="">Foto</label>
-                <input minlength="11" maxlength="255" type="text" name='pic' id='pic' class='form-control' placeholder="URL" required>
+                <input minlength="11" maxlength="255" type="text" name='pic' id='pic' class='form-control' placeholder="URL" value="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" required>
                 <br>
                 <label for="course">Curso:</label>
                     <select id="course" name="course">
--- crud/resources/views/students/edit.blade.php
+++ crud/resources/views/students/edit.blade.php
@@ -17,24 +17,17 @@
             <form action="{{url('student/'.$students->id)}}" method='post'>
                     {!! csrf_field() !!}
                     @method('PATCH')
-                    {{-- <input type="hidden" name='id' id='id' value='{{$students->id}}'>
-                    <label for="">Nome</label>
-                    <input type="text" name='name' id='name' value='{{$students->name}}' class='form-control' required>
-                    <br> --}}
-                    <label for="">Endereço</label>
-                    <input type="text" name='adress' id='adress' value='{{$students->adress}}' class='form-control' required>
-                    <br>
                     <label for="">E-mail</label>
-                    <input type="email" name='email' id='email' value='{{$students->email}}' class='form-control' placeholder="@" required>
+                    <input minlength="3" maxlength="255" type="email" name='email' id='email' value='{{$students->email}}' class='form-control' placeholder="exemplo@email.com" required>
                     <br>
                     <label for="">Telefone</label>
-                    <input type="text" name='mobile' id='mobile' value='{{$students->mobile}}' class='form-control' required>
+                    <input minlength="11" maxlength="14" type="text" name='mobile' id='mobile' value='{{$students->mobile}}' class='form-control' required>
                     <br>
-                    {{-- <label for="">CPF</label>
-                    <input type="text" name='cpf' id='cpf' value='{{$students->cpf}}' class='form-control' required>
-                    <br> --}}
+                    <label for="">Endereço</label>
+                    <input minlength="6" maxlength="255" type="text" name='adress' id='adress' value='{{$students->adress}}' class='form-control' required>
+                    <br>
                     <label for="">Foto</label>
-                    <input type="text" name='pic' id='pic' value='{{$students->pic}}' class='form-control' placeholder="URL" required>
+                    <input type="text" name='pic' id='pic' value='{{$students->pic}}' class='form-control' placeholder="URL" value="{{$students->pic}}" required>
                     <br>
                     <label for="course">Curso:</label>
                     <select id="course" name="course">
